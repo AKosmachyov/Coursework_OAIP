0d7f999a98810c552b7598bc67e9adcc574a841c
ALexander <pitazek@gmail.com>
2016-04-14 11:44:10 +0300

    add new Form

--- ui_test/Project1.dpr
+++ ui_test/Project1.dpr
@@ -3,13 +3,15 @@ program Project1;
 uses
   Forms,
   Unit1 in 'Unit1.pas' {Form1},
-  Storage in 'unit/Storage.pas',
-  entitySet in 'unit/entitySet.pas';
+  Storage in 'unit\Storage.pas',
+  entitySet in 'unit\entitySet.pas',
+  TestVieu in 'TestVieu.pas' {Form2};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TForm2, Form2);
   Application.Run;
 end.
